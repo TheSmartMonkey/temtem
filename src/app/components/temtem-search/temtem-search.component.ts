330f02054dc0bca503784f79b671ed84a883dff5
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2021-04-21 15:48:54 +0200

    simple table of all temtems

--- src/app/components/temtem-search/temtem-search.component.ts
+++ src/app/components/temtem-search/temtem-search.component.ts
@@ -1,4 +1,11 @@
+import { TEMTEMS } from './../../models/temtem';
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface Temtem {
+  name: string;
+  type: string;
+}
 
 @Component({
   selector: 'app-temtem-search',
@@ -7,9 +14,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemtemSearchComponent implements OnInit {
 
+  TABLE_ELEMENTS: Temtem[] = [];
+  displayedColumns: string[] = ['name', 'type'];
+  dataSource: any
+
   constructor() { }
 
   ngOnInit(): void {
+    this.createTemtemData();
+    this.dataSource = new MatTableDataSource(this.TABLE_ELEMENTS);
+  }
+
+  createTemtemData() {
+    for (const [key, value] of Object.entries(TEMTEMS)) {
+      const element: Temtem = {
+        name: key,
+        type: value.toString()
+      }
+      this.TABLE_ELEMENTS.push(element)
+    }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,7 +1,3 @@
-import { TemtemTableComponent } from './../../components/temtem-table/temtem-table.component';
-import { MatButtonModule } from '@angular/material/button';
-import { TemtemTypesComponent } from './../../components/temtem-types/temtem-types.component';
-import { TemtemSearchComponent } from './../../components/temtem-search/temtem-search.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -9,6 +5,17 @@ import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../../shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+
+// Pages
+import { TemtemTableComponent } from './../../components/temtem-table/temtem-table.component';
+import { TemtemTypesComponent } from './../../components/temtem-types/temtem-types.component';
+import { TemtemSearchComponent } from './../../components/temtem-search/temtem-search.component';
+
+// Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -21,7 +28,10 @@ import { SharedModule } from '../../shared/shared.module';
     CommonModule,
     SharedModule,
     HomeRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ]
 })
 export class HomeModule {}
