7c357f86bbb60d4dfd0d9a947724231828d3481f
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2021-04-26 15:17:32 +0200

    add new icon

--- main.ts
+++ main.ts
@@ -31,6 +31,7 @@ function createWindow(): BrowserWindow {
       contextIsolation: false,  // false if you want to run 2e2 test with Spectron
       enableRemoteModule : true // true if you want to run 2e2 test  with Spectron or use remote module in renderer context (ie. Angular)
     },
+    icon: path.join(__dirname, 'src/assets/icons/favicon.ico')
   });
 
   if (serve) {
